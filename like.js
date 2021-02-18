0a61a78f8507102b5cddbf386594b716c3ee05ba
Baddhet <Tenjibiu@gmail.com>
2021-02-18 10:33:50 +0300

    scss and like button

--- like.js
+++ like.js
@@ -1,12 +1,15 @@
 let like = document.querySelector('.like-button__heart');
 let likesNumber = document.querySelector('.likes-number');
+let favorite = document.querySelector('.favorite');
 
 like.onclick = function () {
     if(like.classList.contains('like_added')) {
         likesNumber.textContent--;
+        document.getElementById("favorite").innerHTML = "favorite_border";
     }
     else {
         likesNumber.textContent++;
+        document.getElementById("favorite").innerHTML = "favorite";
     }
     like.classList.toggle('like_added');
     likesNumber.classList.toggle('like_added');
